c8aa3372befdf6e73e8e2ac8d452d77ba9ae5971
basic\Paul <paul950104@gmail.com>
2023-10-16 18:32:29 +0900

    fix/feat: 엑셀 다운로드 추가, 스케줄 고치기 추가, 스케줄 초기화 추가, css 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
 import Header from "./page/Header";
+import * as XLSX from 'xlsx';
 import "./App.css";
 
 const App = () => {
@@ -11,20 +12,18 @@ const App = () => {
   const [schedule, setSchedule] = useState([]);
   const [showSchedule, setShowSchedule] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  // 근무 스케쥴 규칙
-  // 1. 하루의 최소근무자 (leastworker) 는 반드시 설정한 수 이상
-  // 2. 하루의 최대근무자 (totalworker) 는 반드시 설정한 수 이하
-  // 3. 연속 휴무는 최대 2일
+  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 1);
 
   useEffect(() => {
     if (showSchedule) {
+      const daysInMonth = new Date(new Date().getFullYear(), selectedMonth, 0).getDate();
+      setTotalday(daysInMonth);
+
       const newSchedule = Array.from({ length: totalworker }, () =>
-        Array.from({ length: totalday }, () => Math.random() < 0.5)
+        Array.from({ length: daysInMonth }, () => Math.random() < 0.5)
       );
-  
       // 각 날짜에 대한 스케줄을 계산
-      for (let i = 0; i < totalday; i++) {
+      for (let i = 0; i < daysInMonth; i++) {
         let workingCount = 0;
         for (let j = 0; j < totalworker; j++) {
           if (newSchedule[j][i]) {
@@ -59,12 +58,12 @@ const App = () => {
       // 연속으로 쉬는 날 조절
       for (let i = 0; i < totalworker; i++) {
         let consecutiveOffDays = 0;
-        for (let j = 0; j < totalday; j++) {
+        for (let j = 0; j < daysInMonth; j++) {
           if (!newSchedule[i][j]) {
             consecutiveOffDays++;
             // 연속으로 쉬는 날은 2일
             if (consecutiveOffDays >= 2) {
-              if (j + 1 < totalday) {
+              if (j + 1 < daysInMonth) {
                 newSchedule[i][j + 1] = true;
                 consecutiveOffDays = 0;
               }
@@ -78,12 +77,12 @@ const App = () => {
       // 연속으로 일하는 날 제한
       for (let i = 0; i < totalworker; i++) {
         let consecutiveWorkingDays = 0;
-        for (let j = 0; j < totalday; j++) {
+        for (let j = 0; j < daysInMonth; j++) {
           if (newSchedule[i][j]) {
             consecutiveWorkingDays++;
             // 연속으로 일하는 날이 5일을 초과하는 경우
             if (consecutiveWorkingDays > 4) {
-              if (j + 1 < totalday) {
+              if (j + 1 < daysInMonth) {
                 newSchedule[i][j + 1] = false;
                 consecutiveWorkingDays = 0;
               }
@@ -97,10 +96,17 @@ const App = () => {
     } else {
       setSchedule([]);
     }
-  }, [totalworker, totalday, showSchedule, leastworker, maximumworker]);
+
+  }, [totalworker, showSchedule, leastworker, maximumworker, selectedMonth, totalday]);
 
   // 스케줄 생성 버튼
   const generateSchedule = () => {
+    if (leastworker === 19 && maximumworker === 96) {
+      alert("오민영 바보")
+    }
+    if (leastworker === 19 && maximumworker === 95) {
+      alert("이효상 천재")
+    }
     if (leastworker > maximumworker) {
       alert('최소 출근자가 최대 출근자보다 많습니다.');
       return;
@@ -113,20 +119,40 @@ const App = () => {
       alert('최대 출근자가 전체 직원보다 많습니다.');
       return;
     }
-    if (totalday > 31) {
-      alert('총 날짜는 31일보다 많을 수 없습니다.');
-      return;
-    }
-    if (totalday < 28) {
-      alert('총 날짜는 28일보다 적을 수 없습니다.');
-      return;
-    }
     setShowSchedule(true);
   };
 
   const resetSchedule = () => {
     setSchedule([]);
     setShowSchedule(false);
+  };
+
+  // 엑셀 다운로드 로직
+  const downloadExcel = (data) => {
+    const worksheet = XLSX.utils.aoa_to_sheet([
+      ...data.map((workerSchedule, index) =>
+        ["직원 " + (index + 1), ...workerSchedule.map(day => day ? "출" : "휴")]
+      )
+    ]);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Schedule');
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    saveAsExcelFile(excelBuffer, '직원 스케줄');
+  };
+  
+  const saveAsExcelFile = (buffer, fileName) => {
+    const data = new Blob([buffer], { type: 'application/octet-stream' });
+    const url = window.URL.createObjectURL(data);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', `${fileName}.xlsx`);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+  
+  const downloadScheduleAsExcel = () => {
+    downloadExcel(schedule);
   };
 
   return (
@@ -141,8 +167,8 @@ const App = () => {
               value={leastworker}
               onChange={(e) => {
                 const value = parseInt(e.target.value);
-                if (value === '' || (value !== '' && !isNaN(value))) {
-                  setLeastworker(value === '' ? 0 : parseInt(value));
+                if (!isNaN(value)) {
+                  setLeastworker(value);
                 }
               }}
               className="leastinput"
@@ -156,101 +182,112 @@ const App = () => {
               value={maximumworker}
               onChange={(e) => {
                 const value = parseInt(e.target.value);
-                if (value === '' || (value !== '' && !isNaN(value))) {
-                  setMaximumworker(value === '' ? 0 : parseInt(value));
+                if (!isNaN(value)) {
+                  setMaximumworker(value);
                 }
               }}
               className="maximuminput"
             />{" "}
             명
           </div>
-          {/* 총 날짜 */}
-          <div className="totalday">
-            이번 달은 총
-            <input
-              value={totalday}
-              onChange={(e) => {
-                const value = parseInt(e.target.value);
-                if (value === '' || (value !== '' && !isNaN(value))) {
-                  setTotalday(value === '' ? 0 : parseInt(value));
-                }
-              }}
-              className="totaldayinput"
-            />{" "}
-            일
-          </div>
-          {/* 근무하는 직원 */}
+          {/* 총 직원 */}
           <div className="totalworker">
-            근무하는 직원
+            총 직원
             <input
               value={totalworker}
               onChange={(e) => {
                 const value = parseInt(e.target.value);
-                if (value === '' || (value !== '' && !isNaN(value))) {
-                  setTotalworker(value === '' ? 0 : parseInt(value));
+                if (!isNaN(value)) {
+                  setTotalworker(value);
                 }
               }}
               className="totalworkerinput"
             />{" "}
             명
           </div>
+          {/* 날짜 선택 */}
+          <div className="selectedDate">
+            이번 달은
+            <input
+              value={selectedMonth}
+              onChange={(e) => {
+                const value = parseInt(e.target.value);
+                if (!isNaN(value)) {
+                  setSelectedMonth(value);
+                }
+              }}
+              className="totalselectedDate"
+            />{" "}
+            월
+          </div>
         </div>
         <div className="schedulebtndiv">
-            <button className="howtousebtn" onClick={() => setModalIsOpen(true)}>
-              사용방법
-            </button>
-            <button className="schedulebtn" onClick={generateSchedule}>스케줄 확인</button>
+          <button className="howtousebtn" onClick={() => setModalIsOpen(true)}>
+            사용방법
+          </button>
+          <button className="schedulebtn" onClick={generateSchedule}>스케줄 확인</button>
           {showSchedule && (
-            <button className="schedulebtn" onClick={resetSchedule}>
+            <div>
+              <button className="schedulebtn" onClick={resetSchedule}>
               스케줄 초기화
-            </button>
+              </button>
+              <button className="schedulebtn" onClick={downloadScheduleAsExcel}>
+              엑셀 다운받기
+              </button>
+            </div>
           )}
         </div>
         <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
           <div className="howtousemodaldiv">
+            {/* 모달 내용 */}
             <h1>* 스케줄 자동 배정 시스템 사용 방법 *</h1>
             <br/>
             <p>* 사용방법 *</p>
-            <p>1. 일 최소 출근은 일 최대 출근 보다 적을 수 없습니다.</p>
-            <p>2. 일 최소 출근과 일 최대 출근은 근무하는 직원보다 많을 수 없습니다.</p>
-            <p>3. 이번 달은 28보다 적을 수 없고 31보다 많을 수 없습니다.</p>
-            <p>4. 스케줄 확인 후 스케줄 초기화를 누르고 다시 스케줄 확인을 누르면 설정한 상태
-            그대로 다시 스케줄을 만들 수 있습니다.
-            </p>
+            <p>1. 일 최소 출근, 일 최대 출근, 총 직원, 이번 달을 적고 스케줄 확인을 누릅니다.</p>
+            <p>2. 일 최소 출근과 일 최대 출근은 총 직원보다 많을 수 없습니다.</p>
+            <p>3. 스케줄 초기화를 누르고 다시 스케줄 확인을 누르면 설정한 상태 그대로 다시 스케줄을 만들 수 있습니다.</p>
+            <p>4. 적당히 마음에 드는 스케줄을 찾으면 수정해서 사용합니다.</p>
+            <p>5. 수정이 완료되면 엑셀을 다운받아 사용합니다.</p>
             <br/>
             <p>* 참고한 근무 스케줄 규칙 *</p>
-            <p>1. 하루의 최소근무자는 설정한 수 이상</p>
-            <p>2. 하루의 최대근무자는 설정한 수 이하</p>
+            <p>1. 하루의 최소 출근자는 설정한 수 이상</p>
+            <p>2. 하루의 최대 출근자는 설정한 수 이하</p>
             <p>3. 연속 휴무는 최대 2일</p>
             <p>4. 연속 근무는 최대 5일</p>
             <br/>
             <p>* 현재 수정 못한 문제점 *</p>
-            <p>1. 숫자를 지우고 처음부터 다시 적으려면 마우스 드래그 하고 숫자적기</p>
-            <p>2. 월을 설정하면 자동으로 최대 출근일 나오기 수정 예정</p>
-            <p>3. 평일, 주말 반영 수정 예정</p>
-            <p>4. 자잘하게 틀린 경우가 있습니다. 그럴 경우 초기화 및 확인</p>
+            <p>1. 숫자를 지우고 처음부터 다시 적으려면 마우스 드래그 하고 숫자를 적어 주세요.</p>
+            <p>2. 자잘하게 틀린 경우가 있습니다. 그럴 경우 초기화 및 확인을 눌러 주세요.</p>
           </div>
           <div className="howtousebtndiv" >
             <button className="howtousebtn" onClick={() => setModalIsOpen(false)}>닫기</button>
           </div>
         </Modal>
         {showSchedule && (
-        <div className="schedule">
-        {schedule.map((workerSchedule, index) => (
-          <div key={index} className="worker-schedule">
-            <div className="schedule-label">직원 {index + 1} :</div>
-            {workerSchedule.map((isWorking, dayIndex) => (
-              <div
-                key={dayIndex}
-                className={isWorking ? "working-day" : "off-day"}
-              >
-                {isWorking ? "출" : "휴"}
+          <div className="schedule">
+            {schedule.map((workerSchedule, index) => (
+              <div key={index} className="worker-schedule">
+                <div className="schedule-label">직원 {index + 1} :</div>
+                {workerSchedule.map((isWorking, dayIndex) => (
+                  <select
+                    key={dayIndex}
+                    className={isWorking ? "working-day" : "off-day"}
+                    value={isWorking ? "출" : "휴"}
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      const updatedSchedule = [...schedule];
+                      updatedSchedule[index][dayIndex] = value === "출";
+                      setSchedule(updatedSchedule);
+                    }}
+                  >
+                    <option className="off-day" value="휴">휴</option>
+                    <option className="working-day" value="출">출</option>
+                  </select>
+                ))}
               </div>
             ))}
-          </div>
-        ))}
-      </div>      
-      )}
+          </div>      
+        )}
       </div>
     </div>
   );
