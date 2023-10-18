738e644de7f5e4f14d27890a01c1171e4acfbe3a
basic\Paul <paul950104@gmail.com>
2023-10-18 14:38:00 +0900

    fix: fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../src/style/reset.css';
-import App from "./app.js"
+import App from "./App.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
